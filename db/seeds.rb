2bb1a9b9e6da10089cc1d079139dd44785c4ddef
Carlos Sánchez Pérez <carloscsanchezperez@gmail.com>
2020-09-13 18:40:52 +0200

    add user and token create and login

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(username: "csp@gmail.com", password_digest: "1234567890")
